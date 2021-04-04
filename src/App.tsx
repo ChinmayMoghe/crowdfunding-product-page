c8147d271349128b507b6958e9d38a43a72a40f7
chinmay <chinmay11.moghe@gmail.com>
2021-04-04 17:34:20 +0530

    Theme changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import {
   ProjectInfo,
   ProjectFunding,
   ProjectDescription,
+  Navbar,
 } from './components';
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { theme } from './theme';
 
 const GlobalStyle = createGlobalStyle`
   *,
@@ -15,23 +17,29 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding:0;
     box-sizing:border-box;
+    font-family: 'Commissioner', sans-serif;
   }
   html {
-    font-size: 16px;
-    font-family: 'Commissioner', sans-serif;
+    font-size: 62.5%;
+  }
+  body {
+    width: 100vw;
   }
 `;
 
 const App = () => {
   return (
     <>
-      <GlobalStyle />
-      <Container>
-        <Header />
-        <ProjectInfo />
-        <ProjectFunding />
-        <ProjectDescription />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Container>
+          <Header />
+          <Navbar />
+          <ProjectInfo />
+          <ProjectFunding />
+          <ProjectDescription />
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
